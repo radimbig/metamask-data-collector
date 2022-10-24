366d7d0406cd7fee0adeed382e0a51810e39e9e4
radimbig <radimvadim@gmail.com>
2022-10-24 19:19:16 +0200

    login, show address/balance

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", accountsChanged);
+      window.ethereum.on("chainChanged", chainChanged);
+    }
+  }, [balance]);
+
+  const connectHandler = async () => {
+    if (window.ethereum) {
+      try {
+        const res = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        await accountsChanged(res[0]);
+      } catch (err) {
+        console.error(err);
+        setErrorMessage("There was a problem connecting to MetaMask");
+      }
+    } else {
+      setErrorMessage("Install MetaMask");
+    }
+  };
+
+  const accountsChanged = async (newAccount) => {
+    setAccount(newAccount);
+    try {
+      const balance = await window.ethereum.request({
+        method: "eth_getBalance",
+        params: [newAccount.toString(), "latest"],
+      });
+      setBalance(ethers.utils.formatEther(balance));
+    } catch (err) {
+      alert(err);
+      setErrorMessage("There was a problem connecting to MetaMask");
+    }
+  };
+
+  const chainChanged = () => {
+    connectHandler();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>your adress is {account}</div>
+      <div>your balance is {balance}</div>
+      <button style={{height:"45px"}} onClick={connectHandler}>connect Metamask!</button>
     </div>
   );
 }
